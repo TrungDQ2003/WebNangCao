f43f8e790e3bce1152ba5a587a73ee5c0256d37c
TrungDQ2003 <doqt2003@gmail.com>
2024-06-20 23:52:00 +0700

    Show Order Data in Admin Panel Fix

--- Laravel Project/storage/framework/views/256e3141e280a9ae0c04fb42f5a21dc273ec6a98.php
+++ Laravel Project/storage/framework/views/256e3141e280a9ae0c04fb42f5a21dc273ec6a98.php
@@ -89,6 +89,14 @@
               <span class="menu-title">Category</span>
             </a>
           </li>
+          <li class="nav-item menu-items">
+            <a class="nav-link" href="<?php echo e(url('order')); ?>">
+              <span class="menu-icon">
+                <i class="mdi mdi-playlist-play"></i>
+              </span>
+              <span class="menu-title">Order</span>
+            </a>
+          </li>
           
         </ul>
       </nav><?php /**PATH C:\GitHub\WebNangCao\Laravel Project\resources\views/admin/sidebar.blade.php ENDPATH**/ ?>
