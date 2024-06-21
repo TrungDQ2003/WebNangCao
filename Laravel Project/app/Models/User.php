97526d196fa3eda94c7b036b0753711e952d3b15
TrungDQ2003 <doqt2003@gmail.com>
2024-06-22 06:56:56 +0700

    Add more sample product and undo the newest commit

--- Laravel Project/app/Models/User.php
+++ Laravel Project/app/Models/User.php
@@ -27,7 +27,7 @@ class User extends Authenticatable implements MustVerifyEmail
         'name',
         'email',
         'phone',
-        'address',
+        'adress',
         'password',
     ];
 
